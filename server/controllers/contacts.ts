362154fe0441cebfbc033d1ad9a25609f97a0ca0
sofi.sher <sofi.sheromova@gmail.com>
2021-04-26 22:34:28 +0500

    fix build

--- server/controllers/contacts.ts
+++ server/controllers/contacts.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import User from 'models/user';
+import User from '../models/user';
 
 export function list(_req: Request, res: Response): void {
     // eslint-disable-next-line no-warning-comments
