50a170718a0296ae1a0c3b21e1ff54683e02a98b
sofi.sher <sofi.sheromova@gmail.com>
2021-04-27 00:08:39 +0500

    linting for .tsx files and fix lint warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-import App, { Container, AppContext } from 'next/app';
-import React from 'react';
+import App, { Container, AppContext, AppInitialProps } from 'next/app';
 
 import './app.css';
 
 export default class MyApp extends App {
-    static async getInitialProps({ Component, ctx }: AppContext) {
+    static async getInitialProps({ Component, ctx }: AppContext): Promise<AppInitialProps> {
         let pageProps = {};
 
         if (Component.getInitialProps) {
@@ -14,7 +13,7 @@ export default class MyApp extends App {
         return { pageProps };
     }
 
-    render() {
+    render(): JSX.Element {
         const { Component, pageProps } = this.props;
 
         return (
--- server/middlewares/render.ts
+++ server/middlewares/render.ts
@@ -1,7 +1,7 @@
 import { NextFunction as Next, Request, Response } from 'express';
 import { NextServer } from 'next/dist/server/next';
 
-export default (nextApp: NextServer) => (req: Request, res: Response, next: Next) => {
+export default (nextApp: NextServer) => (req: Request, res: Response, next: Next): void => {
     req.nextApp = nextApp;
     res.renderPage = (pathname, query) => {
         nextApp.render(req, res, pathname, query);
