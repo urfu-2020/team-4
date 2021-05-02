fce371b4e4f87e00ec0d1baef4dab76c6d6b0868
Gleb1nho <55406035+Gleb1nho@users.noreply.github.com>
2021-05-02 16:50:29 +0600

    Updated db link
    Поменял ссылку на бд, подходящей под модель старого формата. В будущем ссылка поменяется. Деплой должен ожить.

--- server/databaseConfig/database.ts
+++ server/databaseConfig/database.ts
@@ -2,7 +2,8 @@ import { Sequelize } from 'sequelize';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+// Ссылка на бд в будущем изменится, это старый формат
 // eslint-disable-next-line max-len
-const dbUrl = `postgres://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@dumbo.db.elephantsql.com:5432/${process.env.DB_ADMIN}`;
+const dbUrl = `postgres://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@hattie.db.elephantsql.com:5432/${process.env.DB_ADMIN}`;
 
 export const db = new Sequelize(dbUrl);
