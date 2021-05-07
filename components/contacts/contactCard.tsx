ba67c904666f320e90da91f39d0c461624df843d
sofi.sher <sofi.sheromova@gmail.com>
2021-05-08 00:31:55 +0500

    contacts page

--- components/contacts/contactCard.tsx
+++ components/contacts/contactCard.tsx
@@ -1,23 +1,26 @@
 import { IUserData } from '../../server/types';
 import styles from './index.module.css';
 import Link from 'next/link';
+import classNames from 'classnames';
 
 interface IContactProps {
     user: IUserData
 }
 
-export default function ContactCard({ user }: IContactProps) : JSX.Element {
+export default function ContactCard({ user }: IContactProps): JSX.Element {
     const linkHref = { pathname: '/chat', query: { id: user.id } };
 
     return (
-        <li className={styles.userCard} key={user.id}>
-            <div className={styles.avatarContainer}>
-                <img src={user.avatar} alt="avatar"
-                    className={styles.avatarImage}/>
-            </div>
-            <Link as={`/contacts/${user.id}`} href={linkHref}>
-                <a className={styles.userName}>{user.nickname}</a>
-            </Link>
-        </li>
+        <Link as={`/contacts/${user.id}`} href={linkHref}>
+            <li className={styles.userCard} key={user.id}>
+                <div className={classNames(styles.avatarContainer, styles.column)}>
+                    <img src={user.avatar} alt="avatar"
+                        className={styles.avatarImage}/>
+                </div>
+                <div className={classNames(styles.infoContainer, styles.column)}>
+                    <span className={styles.userName}>{user.nickname}</span>
+                </div>
+            </li>
+        </Link>
     );
 }
