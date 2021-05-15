5397e03dad52b3f3d517e6dd26be25b162fb5763
sofi.sher <sofi.sheromova@gmail.com>
2021-05-15 15:35:38 +0500

    static from surge

--- components/loader/index.tsx
+++ components/loader/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function Loader(): JSX.Element {
     return <div className="loader-container">
         <Head>
-            <link rel="stylesheet" href="/loader.css"/>
+            <link rel="stylesheet" href="http://kilogram-team-4.surge.sh/loader.css"/>
         </Head>
         <div className="lds-ellipsis"><div/><div/><div/><div/></div>
     </div>;
