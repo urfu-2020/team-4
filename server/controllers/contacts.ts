a4d3f2a4979dc61302e4eb34ddf3f628969e61ea
Gleb1nho <55406035+Gleb1nho@users.noreply.github.com>
2021-05-16 21:45:46 +0600

    Update contacts.ts

--- server/controllers/contacts.ts
+++ server/controllers/contacts.ts
@@ -5,8 +5,8 @@ export function list(_req: Request, res: Response): void {
     user.findAll().then(users => {
         const allUsers = [];
         for (const usr of users) {
-            const nickname = usr.getDataValue('githubid');
-            const avatar = `https://avatars.githubusercontent.com/${nickname}`;
+            const nickname = usr.getDataValue('githubLogin');
+            const avatar = usr.getDataValue('avatar');
             allUsers.push({ nickname, avatar });
         }
         res.json({ contacts: allUsers });
@@ -16,7 +16,7 @@ export function list(_req: Request, res: Response): void {
 
 export function item(req: Request, res: Response): void {
     const { id } = req.params;
-    user.findOne({ where: { githubid: id } }).then(usr => {
+    user.findOne({ where: { githubLogin: id } }).then(usr => {
         res.json(usr);
     })
         .catch(err => console.error(err));
