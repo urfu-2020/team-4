3c302c473c00dcb9c9eb39d172e4bca3b8d73d68
Gleb1nho <55406035+Gleb1nho@users.noreply.github.com>
2021-05-16 17:24:32 +0600

    Update auth.ts

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -49,8 +49,7 @@ export default (app: Application): void => {
         user.findOrCreate({
             where: { githubId: profile._json.login },
             defaults: {
-                avatar: profile._json.avatar_url,
-                email: profile._json.email
+                avatar: profile._json.avatar_url
             }
         })
             .then(() => {
